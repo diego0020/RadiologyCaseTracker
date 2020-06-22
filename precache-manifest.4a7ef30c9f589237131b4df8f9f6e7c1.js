e47a45c0897ea59625e65555c41cd8acbce01eba
diego <gus0020@gmail.com>
2020-06-22 16:24:55 -0500

    Updates

---
+++ precache-manifest.4a7ef30c9f589237131b4df8f9f6e7c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea490ce7481fc91e84847f85c47dc182",
+    "url": "/RadiologyCaseTracker/index.html"
+  },
+  {
+    "revision": "705140a18260d372c84a",
+    "url": "/RadiologyCaseTracker/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "f90ac29ea4963213d4c7",
+    "url": "/RadiologyCaseTracker/static/css/main.29b0924c.chunk.css"
+  },
+  {
+    "revision": "705140a18260d372c84a",
+    "url": "/RadiologyCaseTracker/static/js/2.1da64836.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/RadiologyCaseTracker/static/js/2.1da64836.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f90ac29ea4963213d4c7",
+    "url": "/RadiologyCaseTracker/static/js/main.7fb0260b.chunk.js"
+  },
+  {
+    "revision": "a68c1cf43d01b71e2b19",
+    "url": "/RadiologyCaseTracker/static/js/runtime-main.c3f60ef7.js"
+  }
+]);
